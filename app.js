bf682211e79c27e93d59e4dc3936394605a86473
coder7475 <robiulhossain7475@gmail.com>
2024-01-09 18:51:22 +0600

    Feature: Main Game function Create game function where we decide best out of 5 games. Created first version that prints winner for 5 rounds.

--- app.js
+++ app.js
@@ -12,24 +12,44 @@ function getComputerChoice() {
 function singleRound(playerSelection, computerSelection) {
   const ps = playerSelection.toLowerCase();
 
-  if (ps === computerSelection)
+  if (ps === computerSelection){
     return "Tie! Play again!";
+  }
   else if (ps == "rock") {
     if (computerSelection == "paper") 
-      return `You Win! Rock beats Paper`;
+      return [`You Win! Rock beats Paper!!`, true];
     else
-      return "You Lose! Scissor beats Rock";
+      return ["You Lose! Scissor beats Rock!!", false];
   }
   else if (ps == "paper") {
     if (computerSelection == "rock") 
-      return `You Win! Paper beats Rock`;
+      return [`You Win! Paper beats Rock`, true];
     else
-      return "You Lose! Scissor beats Papper";
+      return ["You Lose! Scissor beats Papper!!", false];
   }
   else if (ps == "scissor") {
     if (computerSelection == "paper") 
-      return `You Win! Scissor beats Paper`;
+      return [`You Win! Scissor beats Paper!!`, true];
     else
-      return "You Lose! Rock beats scissor";
+      return ["You Lose! Rock beats scissor!!", false];
+  }
+  else {
+    return ["Invalid Choice", null];
   }
 }
+
+//? Play a session where people win best out of 5
+function game() {
+  
+  for (let i = 0; i < 5; i++) {
+    const playerChoice = prompt("Your choice: ");
+    const computerChoice = getComputerChoice();
+
+    const res = singleRound(playerChoice, computerChoice);
+
+    console.log(res[0]);
+  }
+
+}
+
+game();
