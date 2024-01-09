bb78b70dcd18b80c14e921a0ee8331b8bdac82a2
coder7475 <robiulhossain7475@gmail.com>
2024-01-09 18:06:05 +0600

    feat: create a function called single Round that returns the result of one round of our game

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
+//? random computer choice
+const choices = ["rock", "paper", "scissor"];
+
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissor"];
   const randomIndex = Math.round(Math.random()*2);
   // console.log(choices[randomIndex]);
   return choices[randomIndex];
 }
 
-getComputerChoice();
+// getComputerChoice();
+//? write a function that plays a single round
+function singleRound(playerSelection, computerSelection) {
+  const ps = playerSelection.toLowerCase();
+
+  if (ps === computerSelection)
+    return "Tie! Play again!";
+  else if (ps == "rock") {
+    if (computerSelection == "paper") 
+      return `You Win! Rock beats Paper`;
+    else
+      return "You Lose! Scissor beats Rock";
+  }
+  else if (ps == "paper") {
+    if (computerSelection == "rock") 
+      return `You Win! Paper beats Rock`;
+    else
+      return "You Lose! Scissor beats Papper";
+  }
+  else if (ps == "scissor") {
+    if (computerSelection == "paper") 
+      return `You Win! Scissor beats Paper`;
+    else
+      return "You Lose! Rock beats scissor";
+  }
+}
